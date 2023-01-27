8a06dd906949e7da7add822e94f0cab7d361489a
Dulce-Diaz23 <123528632+Dulce-Diaz23@users.noreply.github.com>
2023-01-27 00:07:10 -0600

    Ejercicio 1 Lenguaje
    Primer ejercicio

---
+++ IParcial/IParcial/Ejercicio1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Ejercicio1 : Form
+    {
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sumarbutton_Click(object sender, EventArgs e)
+        {
+            // Variables
+
+            decimal numero1 = Convert.ToDecimal(Numero1TextBox.Text);
+            decimal numero2 = Convert.ToDecimal(Numero2TextBox.Text);
+
+            decimal resultado = numero1 + numero2;
+
+            MessageBox.Show("La suma es: " + resultado.ToString());
+        }
+    }
+}
---
+++ PrimerP/PrimerP/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimerP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Variables
+            decimal Numero1 = Convert.ToDecimal(Numero1TextBox.Text);
+            decimal Numero2 = Convert.ToDecimal(Numero2TextBox.Text);
+
+            decimal resultado = Numero1 + Numero2;
+
+            MessageBox.Show("La suma es:" + resultado);
+        }
+    }
+}
