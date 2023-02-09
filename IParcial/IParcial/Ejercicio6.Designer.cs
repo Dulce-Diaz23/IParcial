503ddeb80b8fe12e1f790e932c247cf2cc4ab113
Dulce-Diaz23 <123528632+Dulce-Diaz23@users.noreply.github.com>
2023-02-08 23:42:48 -0600

    Ejercicio No. 6
    Arreglos

---
+++ IParcial/IParcial/Ejercicio6.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace IParcial
+{
+    partial class Ejercicio6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ArreglosListBox = new System.Windows.Forms.ListBox();
+            this.ArregloButton = new System.Windows.Forms.Button();
+            this.ArreglosComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ArreglosListBox
+            // 
+            this.ArreglosListBox.FormattingEnabled = true;
+            this.ArreglosListBox.Location = new System.Drawing.Point(74, 28);
+            this.ArreglosListBox.Name = "ArreglosListBox";
+            this.ArreglosListBox.Size = new System.Drawing.Size(120, 95);
+            this.ArreglosListBox.TabIndex = 0;
+            // 
+            // ArregloButton
+            // 
+            this.ArregloButton.Location = new System.Drawing.Point(259, 74);
+            this.ArregloButton.Name = "ArregloButton";
+            this.ArregloButton.Size = new System.Drawing.Size(90, 30);
+            this.ArregloButton.TabIndex = 1;
+            this.ArregloButton.Text = "Arreglo";
+            this.ArregloButton.UseVisualStyleBackColor = true;
+            this.ArregloButton.Click += new System.EventHandler(this.ArregloButton_Click);
+            // 
+            // ArreglosComboBox
+            // 
+            this.ArreglosComboBox.FormattingEnabled = true;
+            this.ArreglosComboBox.Location = new System.Drawing.Point(74, 146);
+            this.ArreglosComboBox.Name = "ArreglosComboBox";
+            this.ArreglosComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ArreglosComboBox.TabIndex = 2;
+            // 
+            // Ejercicio6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 299);
+            this.Controls.Add(this.ArreglosComboBox);
+            this.Controls.Add(this.ArregloButton);
+            this.Controls.Add(this.ArreglosListBox);
+            this.Name = "Ejercicio6";
+            this.Text = "Ejercicio6";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ArreglosListBox;
+        private System.Windows.Forms.Button ArregloButton;
+        private System.Windows.Forms.ComboBox ArreglosComboBox;
+    }
+}
---
+++ IParcial/IParcial/Ejercicio6.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Ejercicio6 : Form
+    {
+        public Ejercicio6()
+        {
+            InitializeComponent();
+        }
+
+        private void ArregloButton_Click(object sender, EventArgs e)
+        {
+            // Vectores
+            int[]vector = new int[5];
+
+            vector[0] = 10;   //Manera 1 declarar
+            vector[1] = 20;
+            vector[2] = 30;
+            vector[3] = 40;
+            vector[4] = 50;
+
+            int[] vector2 = new int[5] {10, 20, 30, 40, 50};         //Manera 2  declarar
+            int[] vector3 = new int[] { 10, 20, 30, 40, 50, 60 };    // Manera 3 decarar
+            int[] vector4 =  { 10, 20, 30, 40, 50, 60 };             // Manera 4
+
+            string[] vector5 = {"Dulce", "Maria", "Heber", "Manuel"};
+
+            for (int i = 0; i < vector5.Length; i++)
+            {
+                ArreglosListBox.Items.Add(vector5[i]);          //add agrega valor a lixtbox
+            }
+
+            foreach (string item in vector5)                   
+            {
+                ArreglosComboBox.Items.Add(item);
+            }
+        }
+    }
+}
