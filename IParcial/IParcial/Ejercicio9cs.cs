fb5959e91ab713338681e54c65cbfa975330752f
Dulce-Diaz23 <123528632+Dulce-Diaz23@users.noreply.github.com>
2023-02-09 18:32:32 -0600

    Ejercicio No. 9
    POO

---
+++ IParcial/IParcial/Ejercicio9cs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using POO;
+
+namespace IParcial
+{
+    public partial class Ejercicio9cs : Form
+    {
+        public Ejercicio9cs()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Coche miCoche = new Coche();
+            Coche miCoche2 = new Coche("Toyota", "22R");
+        }
+    }
+}
---
+++ IParcial/POO/Coche.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Coche
+    {
+        // atributos siempre deben ser privados
+        private string marca;
+        private string modelo;
+        //private decimal precio;
+
+        //Propiedades
+                                                                               //ayudan a establecer valores a los atributos
+        public string Marca                                                   // manera 1
+        {
+            get { return marca; }                                            //obtener el valor del atributo
+            set { marca = value; }                                          //envia/establece informacion
+        }
+
+        public string Modelo { get => modelo; set => modelo = value; }      // manera 2
+        // manera 3
+        public decimal precio { get; set; }                                 // no es necesario declarar atributo
+        public int kilometro { get; set; }
+
+        
+        //Constuctores, llevan el mismo nomb. de clase
+        public  Coche() { }
+
+        public Coche(string _marca, string _modelo) 
+        {
+            Marca = _marca;
+            Modelo = _modelo;
+
+        }
+
+        public Coche(string marca, string modelo, decimal precio, int kilometro) : this(marca, modelo)
+        {
+            this.precio = precio;
+            this.kilometro = kilometro;
+        }
+
+
+        // Metodos
+        public string DevolverDatosBasic()
+        {
+            return "Marca: " + Marca + "Modelo: " + Modelo + "Precio: " + precio;
+        }
+
+    }
+
+}
